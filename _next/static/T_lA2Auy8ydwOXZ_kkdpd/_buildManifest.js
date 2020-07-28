777c8fca2a7aae7f28ad52e1c7867ff181191422
ChriChan <1198599982@qq.com>
2020-07-28 14:56:33 +0800

    routine update

--- _next/static/T_lA2Auy8ydwOXZ_kkdpd/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = (function(a){return {"/":[a],"/category/[parent]":[a],"/category/[parent]/[child]":[a],"/index":[a]}}("static\u002Fchunks\u002Fc991deed0ecea6a43db0b51d6341bf2ae75a2abc.a861c9cd9f491d2c129c.js"));self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
