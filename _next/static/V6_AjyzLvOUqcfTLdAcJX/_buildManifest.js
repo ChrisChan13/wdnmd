df68ff157bf6a279296d0c69ffcd796948e51194
ChriChan <1198599982@qq.com>
2020-07-28 17:40:00 +0800

    routine update

--- _next/static/V6_AjyzLvOUqcfTLdAcJX/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = (function(a){return {"/":[a],"/category/[parent]":[a],"/category/[parent]/[child]":[a],"/index":[a]}}("static\u002Fchunks\u002F9e99f673382c2d518c51c15caecd0e7943256d7d.a861c9cd9f491d2c129c.js"));self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
---
+++ _next/static/webpack/styles.8c6fd4114e376ed0c3f3.hot-update.js
@@ -0,0 +1,10 @@
+webpackHotUpdate("styles",{
+
+/***/ "./pages/index/index.less":
+false,
+
+/***/ 5:
+false
+
+})
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IiIsImZpbGUiOiJzdGF0aWMvd2VicGFjay9zdHlsZXMuOGM2ZmQ0MTE0ZTM3NmVkMGMzZjMuaG90LXVwZGF0ZS5qcyIsInNvdXJjZVJvb3QiOiIifQ==
