45d8709525165e6ba3d1cb599d4e71cfb270be54
ChriChan <1198599982@qq.com>
2020-08-10 17:36:58 +0800

    routine update

---
+++ _next/static/VAEO3T644yPLf4jVTze5t/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = (function(a){return {"/":[a],"/index":[a],"/post/[id]":[a],"/tag/[parent]":[a],"/tag/[parent]/[child]":[a]}}("static\u002Fchunks\u002F459033fb698583286d9673b8a81e8b3e7fe690f4.67739fb2276bebe43544.js"));self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
---
+++ _next/static/fqb565QT61dXle-A-xH8c/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = (function(a){return {"/":[a],"/index":[a],"/post/[id]":[a],"/tag/[parent]":[a],"/tag/[parent]/[child]":[a]}}("static\u002Fchunks\u002F58b9380a120185150714b67bd25a87c244501007.67739fb2276bebe43544.js"));self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
