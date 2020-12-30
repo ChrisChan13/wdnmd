bae04c1ee013c9faf2e427c3651c2a6928fc8115
ChrisChan <chrischan1213@qq.com>
2020-12-30 09:19:55 +0800

    routine update

---
+++ _next/static/jl60RnfuI61BAE8CfAN5m/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = (function(a){return {"/":[a],"/index":[a],"/post/[id]":[a],"/tag/[parent]":[a],"/tag/[parent]/[child]":[a]}}("static\u002Fchunks\u002F27132cf913a5b81b86837323296fbebe989585a4.67739fb2276bebe43544.js"));self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
